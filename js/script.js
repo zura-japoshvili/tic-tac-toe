d25d9c74a822359e05765127ddfe843420f306fe
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-12-03 19:21:00 +0400

    update

--- js/script.js
+++ js/script.js
@@ -46,6 +46,9 @@ function cellPlayed(index, cell){
     }
 }
 
+
+// A random position is created in the "randomPos" function if this position 
+// is occupied it calls "opponentsTurn" which re-calls "randomPos" 
 function opponentsTurn(){
     if(gameState.includes("")){
         randomPos();
@@ -55,7 +58,6 @@ function opponentsTurn(){
         gameActive = false; 
     }
 }
-
 function randomPos(){
     let usedPos = [];
     gameState.forEach((value, index) => {
